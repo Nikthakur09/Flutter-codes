58d03ac5d53bc4713d6e898067d6feca4d0b9cfe
Nikhil Singh <114174800+Nikthakur09@users.noreply.github.com>
2023-04-29 16:50:17 +0530

    Create stream.dart

---
+++ stream.dart
@@ -0,0 +1,34 @@
+//This is something how stream works
+import 'dart:async';
+class Order{
+  String type;
+  Order(this.type);
+}
+class Cake {
+  
+}
+
+
+void main() {
+ final sc = StreamController(); 
+  final baker = StreamTransformer.fromHandlers(
+  handleData: (cakeType,sink){
+    if(cakeType == 'Chocklate'){
+      sink.add(Cake);
+    }else{
+      print('I cant bake this type');
+    }
+    
+  }
+  ); 
+final order = Order('Chocklate'); 
+sc.sink.add(order);
+  
+  sc.stream.map((order) => order.type).
+    transform(baker).
+    listen(
+    onError: (err)=> print(err),
+    (cake) =>  print(cake.runtimeType),
+    
+  );
+}
